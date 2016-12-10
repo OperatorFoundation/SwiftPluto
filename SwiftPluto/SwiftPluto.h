cc223b44bc525defef2b9b6f12fd4daf681ba556
Lita <looklita@gmail.com>
2016-12-09 18:14:56 -0600

    Initial Commit

---
+++ SwiftPluto/SwiftPluto.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPluto.h
+//  SwiftPluto
+//
+//  Created by Adelita Schule on 12/9/16.
+//  Copyright © 2016 Operator Foundation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftPluto.
+FOUNDATION_EXPORT double SwiftPlutoVersionNumber;
+
+//! Project version string for SwiftPluto.
+FOUNDATION_EXPORT const unsigned char SwiftPlutoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPluto/PublicHeader.h>
+
+
